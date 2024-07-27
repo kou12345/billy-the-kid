7a612e007fecc9e8145e9eeff6304dbe330a8994
kou12345 <hi.0412.ko.2002@gmail.com>
2024-07-27 23:10:33 +0900

    add comment

--- src/billy_the_kid/main.py
+++ src/billy_the_kid/main.py
@@ -4,6 +4,8 @@ from billy_the_kid.camera import Camera
 """
 MacBookの場合、0を指定するとエラーになる。とりあえず1を指定すると動作する
 https://github.com/opencv/opencv-python/issues/916
+
+外部カメラを接続している場合は、0を指定する
 """
-camera = Camera(1)
+camera = Camera(0)
 camera.run()
